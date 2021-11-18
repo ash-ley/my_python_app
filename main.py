5e61673d7a11143722dd066d2e1950a9c9f0fea1
ash-ley <ashleyritchie1871@hotmail.co.uk>
2021-11-18 15:34:16 +0000

    UPDATE - Application updated to work through mysql

--- main.py
+++ main.py
@@ -1,41 +1,26 @@
 from flask import Flask, request, Response, render_template
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 import json, random
 
-app = Flask(__name__)
-jokeFile = open("static/jokes.txt")
-# CRUD
-# Try to add it persist between sessions - need to write to the file
-# Main page includes button that calls correct api, maybe do text forms for the other CRUD functions after
-def unicodeDecode(string):
-    string = string.lstrip()
-    string = string.rstrip()
-    string = string.encode()
-    string = string.decode()
-    string = string.replace("\u2019","'")
-    return string
+app = Flask(__name__,instance_relative_config=False)
+app.config.from_object(Config)
 
-def generateJokes(file):
-    jokes = {
-        1 : { 'joke' : 'aa', 'punchline' : 'cc'}
-        }
-    x = 1
-    for line in file:
-        joke, punchline = line.split("<>")
-        jokes[x] = { 'joke': unicodeDecode(joke), 'punchline': unicodeDecode(punchline) }
-        x += 1
-    
-    return jokes
+db = SQLAlchemy(app)
 
-jokes = generateJokes(jokeFile)
-current_id = list(jokes)[-1]
+class Jokes(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True)
+    joke = db.Column(db.VARCHAR(length=255))
+    punchline = db.Column(db.VARCHAR(length=255))
 
 @app.route("/")
 def hello():
     return render_template("index.html")
 
-@app.route("/delete/<id>", methods=['PUT']) # Try as a PUT method or change back to GET
+@app.route("/delete/<id>", methods=['PUT'])
 def delete_joke(id):
-    jokes.pop(int(id))
+    Jokes.query.filter(Jokes.id == id).delete()
+    db.session.commit()
     return "Joke has been deleted"
 
 @app.route("/joke/update/<id>", methods=['GET'])
@@ -43,31 +28,40 @@ def update_joke(id):
     joke = request.args.get('joke', None)
     punchline = request.args.get('punchline', None)
 
-    jokes[int(id)] = {
-        'joke' : joke,
-        'punchline' : punchline
-    }
+    db.session.query(Jokes).filter(Jokes.id==id).update({'joke' : joke,'punchline' : punchline}, synchronize_session=False)
+    db.session.commit()
     return "Joke has been updated"
 
 @app.route("/random")
 def random_joke():
-    return json.dumps(jokes[random.randint(1,int(current_id))])
+    lastID = db.session.query(Jokes).order_by(Jokes.id.desc()).first()
+
+    getJoke = Jokes.query.get(random.randint(1,lastID.id))
+    joke = {
+        getJoke.id : {
+            'joke' : getJoke.joke, 'punchline' : getJoke.punchline
+        }
+    }
+    return json.dumps(joke[getJoke.id])
 
 @app.route("/jokes/<id>", methods=['GET'])
 def get_joke(id):
-    return json.dumps(jokes[int(id)])
+    getJoke = Jokes.query.get(id)
+    joke = {
+        getJoke.id : {
+            'joke' : getJoke.joke, 'punchline' : getJoke.punchline
+        }
+    }
+    return json.dumps(joke[getJoke.id])
 
 @app.route("/joke/add", methods=['POST'])
 def add_joke():
-    global jokes
-    global current_id
-    
     req_data = request.get_json()
     joke = req_data['joke']
     
-    current_id += 1
-    new_joke = { current_id : joke }
-    jokes.update(new_joke)
+    new_joke = Jokes(joke=joke["joke"], punchline=joke["punchline"])
+    db.session.add(new_joke)
+    db.session.commit()
 
     return "Joke was added successfully"
 
